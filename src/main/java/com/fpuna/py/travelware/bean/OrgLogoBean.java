d2169d433a4167fc7fb7641e249530df5b16cb6b
eencina107 <eduardo.encina20@gmail.com>
2015-12-11 14:11:45 -0300

    ABM Organizaciones -  Solucionado el manejo de imagenes.

--- src/main/java/com/fpuna/py/travelware/bean/OrgLogoBean.java
+++ src/main/java/com/fpuna/py/travelware/bean/OrgLogoBean.java
@@ -7,12 +7,23 @@ package com.fpuna.py.travelware.bean;
 
 import com.fpuna.py.travelware.dao.OrganizacionDao;
 import com.fpuna.py.travelware.model.PgeOrganizaciones;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.bean.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseId;
+import javax.inject.Named;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -21,21 +32,46 @@ import org.primefaces.model.StreamedContent;
  * @author eencina
  */
 @ManagedBean
-@ApplicationScoped
-public class OrgLogoBean {
-    @EJB
-    private OrganizacionDao organizacionEJB;
+@RequestScoped
+public class OrgLogoBean implements Serializable{
+    private StreamedContent image;
+
     
-    public StreamedContent getImage(){
-        FacesContext context = FacesContext.getCurrentInstance();
-        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE){
-            return new DefaultStreamedContent();
+    private String orgId;
+
+    @EJB
+    private OrganizacionDao orgEJB;
+    
+    PgeOrganizaciones org;
+    String nombreArch;
+
+    @PostConstruct
+    public void init() {
+        if (FacesContext.getCurrentInstance().getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            image = new DefaultStreamedContent();
         }
-        else
-        {
-            Integer orgId= Integer.parseInt(context.getExternalContext().getRequestParameterMap().get("orgId"));
-            PgeOrganizaciones org= organizacionEJB.getById(orgId);
-            return new DefaultStreamedContent( new ByteArrayInputStream(org.getOrgLogo()));
+        else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            FacesContext context= javax.faces.context.FacesContext.getCurrentInstance();
+            orgId = (context.getExternalContext().getRequestParameterMap().get("orgId")) ;
+            org= orgEJB.getById(Integer.parseInt(orgId));
+            nombreArch = org.getOrgLogo();
+             InputStream stream = null;
+            try {
+                stream = new FileInputStream(new File("/opt/py.travelware/orgLogo/"+org.getOrgLogo()));
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(OrgLogoBean.class.getName()).log(Level.SEVERE, null, ex);
+                FacesContext.getCurrentInstance().addMessage("", new FacesMessage("Error", "No se encuentra el archivo"));
+            }
+            image = new DefaultStreamedContent(stream);
         }
     }
+
+    
+    public StreamedContent getImage() {
+        return image;
+    }
+
+
 }
--- src/main/java/com/fpuna/py/travelware/bean/OrganizacionBean.java
+++ src/main/java/com/fpuna/py/travelware/bean/OrganizacionBean.java
@@ -9,17 +9,24 @@ import com.fpuna.py.travelware.dao.CiudadDao;
 import com.fpuna.py.travelware.dao.OrganizacionDao;
 import com.fpuna.py.travelware.model.PgeCiudades;
 import com.fpuna.py.travelware.model.PgeOrganizaciones;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
+import static org.hibernate.internal.util.io.StreamCopier.BUFFER_SIZE;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.SelectEvent;
@@ -40,6 +47,8 @@ public class OrganizacionBean implements Serializable{
     private CiudadDao ciudadEJB;
     private LoginBean loginBean;
     private PgeCiudades ciudad;
+    String nombreArchivo;
+    String nombreCarpetaImg;
     
     //Crea una nueva instancia
     public OrganizacionBean(){
@@ -57,6 +66,7 @@ public class OrganizacionBean implements Serializable{
         organizacionSelected.setCiuId(ciudad);
         organizaciones = organizacionEJB.getAll();
         ciudades = ciudadEJB.getAll();
+        
     }
 
     private void clean() {
@@ -71,7 +81,7 @@ public class OrganizacionBean implements Serializable{
         FacesContext context = FacesContext.getCurrentInstance();
         String mensaje;
         for (PgeOrganizaciones org:organizaciones){
-            if (org.getOrgDesc().toUpperCase().equals(this.organizacionSelected.getOrgDesc().toUpperCase())){
+            if (org.getOrgDesc().toUpperCase().equals(this.organizacionSelected.getOrgDesc().toUpperCase()) && !Objects.equals(org.getOrgId(), this.organizacionSelected.getOrgId())){
                 context.addMessage(null, new FacesMessage("Advertencia", "La organización "+this.organizacionSelected.getOrgDesc()+" ya existe"));
                 this.clean();
                 return;
@@ -79,28 +89,41 @@ public class OrganizacionBean implements Serializable{
         }
         
         PgeOrganizaciones organizacion = new PgeOrganizaciones();
+        organizacion.setOrgDesc(this.organizacionSelected.getOrgDesc());
         if (this.organizacionSelected.getOrgId()!=null){
             organizacion.setOrgFecMod(new Date());
             organizacion.setOrgUsuMod(loginBean.getUsername());
+            organizacion.setOrgUsuIns(this.organizacionSelected.getOrgUsuIns());
+            organizacion.setOrgFecIns(this.organizacionSelected.getOrgFecIns());
             organizacion.setOrgId(this.organizacionSelected.getOrgId());
             mensaje = organizacion.getOrgDesc()+" fue modificado con éxito!";
         }
         else{
-            organizacion.setOrgFecMod(new Date());
-            organizacion.setOrgUsuMod(loginBean.getUsername());
+            organizacion.setOrgFecIns(new Date());
+            organizacion.setOrgUsuIns(loginBean.getUsername());
             mensaje = organizacion.getOrgDesc()+" fue creado con éxito!";
         }
-        organizacion.setOrgDesc(this.organizacionSelected.getOrgDesc());
+        organizacion.setCiuId(this.organizacionSelected.getCiuId());
         organizacion.setOrgDir(this.organizacionSelected.getOrgDir());
-        organizacion.setOrgLogo(this.organizacionSelected.getOrgLogo());
+        organizacion.setOrgLogo(this.nombreArchivo);
         organizacion.setOrgPagWeb(this.organizacionSelected.getOrgPagWeb());
         organizacion.setOrgSubTipo(this.organizacionSelected.getOrgSubTipo());
         organizacion.setOrgTel(this.organizacionSelected.getOrgTel());
         organizacion.setOrgTipo(this.organizacionSelected.getOrgTipo());
         organizacion.setOrgUbi(this.organizacionSelected.getOrgUbi());
+        organizacionEJB.update(organizacion);
         context.addMessage("Mensaje", new FacesMessage("Felicidades!"+mensaje));
         organizaciones = organizacionEJB.getAll();
         this.clean();        
+    }
+    
+    public void redireccionar(String url) throws IOException{
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Advertencia", "Error al abrir Página"));
+        }
+        
     }
     
     public void deleteOrganizacion(){
@@ -115,15 +138,60 @@ public class OrganizacionBean implements Serializable{
     }
     
     public void handleFileUpload(FileUploadEvent event) {
-        try{
-            this.organizacionSelected.setOrgLogo(event.getFile().getContents());
-        }
-        catch (Exception e){
-            FacesMessage message = new FacesMessage("Error!", event.getFile().getFileName() + e);
-            return;
-        }
-        FacesMessage message = new FacesMessage("Felicidades!", event.getFile().getFileName() + " fue subido con éxito.");
-        FacesContext.getCurrentInstance().addMessage(null, message);
+         ExternalContext extContext = javax.faces.context.FacesContext.getCurrentInstance().getExternalContext();
+         Boolean existeDir; 
+         File targetFolder;
+         File result;
+            try {
+                targetFolder = new File("/opt/py.travelware/orgLogo/");
+                result = new File("/opt/py.travelware/orgLogo/"+event.getFile().getFileName());
+                existeDir = targetFolder.canRead();
+                if (!existeDir){
+                    targetFolder.mkdirs();
+                }
+                //this.getClass().getResource("/").getPath(); 
+                
+                System.out.println("/opt/py.travelware/orgLogo/" + event.getFile().getFileName()+" subido");
+               } catch (Exception e) {
+                   return;
+            }
+
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(result);
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+
+                int bulk;
+                InputStream inputStream = event.getFile().getInputstream();
+                while (true) {
+                    bulk = inputStream.read(buffer);
+                    if (bulk < 0) {
+                        break;
+                    }
+                    fileOutputStream.write(buffer, 0, bulk);
+                    fileOutputStream.flush();
+                }
+
+                fileOutputStream.close();
+                inputStream.close();
+
+                FacesMessage msg = 
+                            new FacesMessage("Propiedades", "Nombre: " +
+                            event.getFile().getFileName() + " Tamaño: " + 
+                            event.getFile().getSize() / 1024 + 
+                            " Kb Tipo: " + 
+                            event.getFile().getContentType() + 
+                                    " Subido correctamente.");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+                this.nombreArchivo = event.getFile().getFileName();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                FacesMessage error = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                               "Error al subir", "");
+                FacesContext.getCurrentInstance().addMessage(null, error);
+            }     
     }
 
     public List<PgeOrganizaciones> getOrganizaciones() {
@@ -172,6 +240,10 @@ public class OrganizacionBean implements Serializable{
 
     public void setCiudad(PgeCiudades ciudad) {
         this.ciudad = ciudad;
+    }
+
+    public String getNombreCarpetaImg(String nombreArchivo) {
+        return nombreCarpetaImg+nombreArchivo;
     }
     
     
