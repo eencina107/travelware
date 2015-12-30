99217e246bbdef42f41112783047ef9f804cc54c
eencina107 <eduardo.encina20@gmail.com>
2015-12-30 10:42:28 -0300

    ABM Usuarios creado

---
+++ src/main/java/com/fpuna/py/travelware/bean/UsuarioBean.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpuna.py.travelware.bean;
+
+import com.fpuna.py.travelware.dao.PersonaDao;
+import com.fpuna.py.travelware.dao.UsuarioDao;
+import com.fpuna.py.travelware.model.PgePersonas;
+import com.fpuna.py.travelware.model.PgeUsuarios;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author eencina
+ */
+@Named(value = "usuarioBean")
+@SessionScoped
+public class UsuarioBean implements Serializable{
+    private List<PgeUsuarios> usuarios;
+    private List<PgePersonas> personas;
+    private PgeUsuarios usuarioSelected;
+    @EJB
+    private UsuarioDao usuarioEJB;
+    @EJB
+    private PersonaDao personaEJB;
+    
+    private LoginBean loginBean;
+    
+    private PgePersonas persona;
+    
+//    crea una nueva instacia de usuario
+    public UsuarioBean(){
+        
+    }
+    
+    @PostConstruct
+    public void init(){
+        clean();
+        FacesContext context = javax.faces.context.FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+        loginBean = (LoginBean) session.getAttribute("loginBean");
+        usuarioSelected = new PgeUsuarios();
+        persona = new PgePersonas();
+        usuarioSelected.setPerId(persona);
+        usuarios = usuarioEJB.getAll();
+        personas = personaEJB.getAll();
+    }
+
+    private void clean() {
+        this.usuarioSelected = new PgeUsuarios();
+    }
+    
+    public void buttonAction(ActionEvent event){
+        this.usuarioSelected = new PgeUsuarios();
+    }
+    
+    public void addUsuario(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        for (PgeUsuarios usu:usuarios){
+            if (usu.getPerId().equals(this.usuarioSelected.getPerId()) && this.usuarioSelected.getUsuId()!=null && !usu.getUsuId().equals(this.usuarioSelected.getUsuId())){
+                context.addMessage(null, new FacesMessage("Advertencia", "Ya existe un usuario para esta persona"));
+                this.clean();
+                return;
+            }
+            if (usu.getUsuCod().equals(this.usuarioSelected.getUsuCod()) && this.usuarioSelected.getUsuId()!=null && !usu.getUsuId().equals(this.usuarioSelected.getUsuId())){
+                context.addMessage(null, new FacesMessage("Advertencia", "Ya existe un usuario con éste codigo"));
+                this.clean();
+                return;
+            }
+        }
+        
+        PgeUsuarios usuario= new PgeUsuarios();
+        if (usuarioSelected.getUsuId()!= null){
+            usuario.setUsuUsuMod(loginBean.getUsername());
+            usuario.setUsuFecMod(new Date());
+            usuario.setUsuUsuIns(usuarioSelected.getUsuUsuIns());
+            usuario.setUsuFecIns(usuarioSelected.getUsuFecIns());
+            usuario.setUsuId(usuarioSelected.getUsuId());
+            usuario.setUsuFecIng(usuarioSelected.getUsuFecIng());
+        }
+        else
+        {
+            usuario.setUsuUsuIns(loginBean.getUsername());
+            usuario.setUsuFecIns(new Date());
+            usuario.setUsuFecIng(new Date());
+        }
+        usuario.setPerId(usuarioSelected.getPerId());
+        usuario.setUsuCargo(usuarioSelected.getUsuCargo());
+        usuario.setUsuCod(usuarioSelected.getUsuCod());
+        usuario.setUsuEst('A');
+        usuario.setUsuPass(usuarioSelected.getUsuPass());
+        usuarioEJB.update(usuario);
+        context.addMessage(null, new FacesMessage("Felicidades!", usuario.getUsuCod()+" fue guardado con éxito"));
+        usuarios=usuarioEJB.getAll();
+        RequestContext.getCurrentInstance().update("usuario-form:dtUsuario");
+        this.clean();
+    }
+    
+    public void deleteUsuario(){
+        usuarioEJB.delete(usuarioSelected);
+        usuarios=usuarioEJB.getAll();
+        RequestContext.getCurrentInstance().update("usuario-form:dtUsuario");
+    }
+    
+    public void onRowSelect(SelectEvent event){
+        this.usuarioSelected = (PgeUsuarios) event.getObject();
+        RequestContext.getCurrentInstance().update("usuario-form:dtUsuario");
+    }
+
+    public List<PgeUsuarios> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<PgeUsuarios> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public List<PgePersonas> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(List<PgePersonas> personas) {
+        this.personas = personas;
+    }
+
+    public PgeUsuarios getUsuarioSelected() {
+        return usuarioSelected;
+    }
+
+    public void setUsuarioSelected(PgeUsuarios usuarioSelected) {
+        this.usuarioSelected = usuarioSelected;
+    }
+
+    public PgePersonas getPersona() {
+        return persona;
+    }
+
+    public void setPersona(PgePersonas persona) {
+        this.persona = persona;
+    }
+    
+    
+}
---
+++ src/main/java/com/fpuna/py/travelware/converters/PersonaConverter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpuna.py.travelware.converters;
+
+import com.fpuna.py.travelware.dao.PersonaDao;
+import com.fpuna.py.travelware.model.PgePersonas;
+import javax.ejb.EJB;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author eencina
+ */
+@FacesConverter(value = "personaConverter")
+public class PersonaConverter implements Converter{
+    @EJB
+    private PersonaDao personaEJB;
+    
+    final static Logger logger= Logger.getLogger(PersonaConverter.class);
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.equals("")){
+            return null;
+        }
+        
+        try {
+            PgePersonas persona = personaEJB.getByNroDoc(Integer.parseInt(value));
+            return persona;
+        } catch (Exception e) {
+            logger.error("CLASS "+this.getClass().getName()+" METHOD: getAsObject ", e);
+            return null;
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (value == null) {
+            return null;
+        }
+        PgePersonas persona = (PgePersonas) value;
+        
+        try {
+            return persona.getPerNroDoc();
+        } catch (Exception e) {
+            logger.error("CLASS "+this.getClass().getName()+" METHOD: getAsString ", e);
+            return null;
+        }
+    }
+    
+}
