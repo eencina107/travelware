8954170dea0c0ca6f8dedc6903cb70bbbe80bf36
DamyGenius <damian.meza.py@hotmail.com>
2016-07-24 11:42:16 -0400

    Se agrega lista de usuarios y sus manejadores

--- src/main/java/com/fpuna/py/travelware/bean/ContactoBean.java
+++ src/main/java/com/fpuna/py/travelware/bean/ContactoBean.java
@@ -6,15 +6,21 @@
 package com.fpuna.py.travelware.bean;
 
 import com.fpuna.py.travelware.dao.ContactoDao;
+import com.fpuna.py.travelware.dao.PersonaDao;
+import com.fpuna.py.travelware.dao.UsuarioDao;
 import com.fpuna.py.travelware.model.ConContactos;
 import com.fpuna.py.travelware.model.PgePersonas;
+import com.fpuna.py.travelware.model.PgeUsuarios;
 import java.io.Serializable;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
 
@@ -25,43 +31,83 @@ import org.primefaces.event.SelectEvent;
 @Named(value = "contactoBean")
 @SessionScoped
 public class ContactoBean implements Serializable{
-    private List<ConContactos> listaContactos;
-    private ConContactos contactoSelected;
-    private List<PgePersonas> listaPersonas;
-    private PgePersonas personaSelected;
-    
+    private List<ConContactos> contactos;
+    private List<PgePersonas> personas;
+    private List<PgeUsuarios> usuarios;
+
     @EJB
     private ContactoDao contactoEJB;
+    @EJB
+    private PersonaDao personaEJB;
+    @EJB
+    private UsuarioDao usuarioEJB;
+
+    private LoginBean loginBean;
+
+    private ConContactos contactoSelected;
+    private PgePersonas personaSelected;
+    private PgeUsuarios usuarioSelected;
     
+//    crea una nueva instacia de contacto
     public ContactoBean(){
+
+    }
+
+    @PostConstruct
+    public void init(){
         clean();
-        this.listaContactos = contactoEJB.getAll();
-        this.contactoSelected = new ConContactos();
+        FacesContext context = javax.faces.context.FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+        loginBean = (LoginBean) session.getAttribute("loginBean");
+        contactoSelected = new ConContactos();
+
+        personaSelected = new PgePersonas();
+        contactoSelected.setConIdPersonaCont(personaSelected);
+        personas = personaEJB.getAll();
+
+        usuarioSelected = new PgeUsuarios();
+        contactoSelected.setConIdUsuarioCont(usuarioSelected);
+        usuarios = usuarioEJB.getAll();
+
+        contactos = contactoEJB.getAll();
     }
 
     private void clean() {
         this.contactoSelected = new ConContactos();
     }
     
+    public void buttonAction(ActionEvent event){
+        this.contactoSelected = new ConContactos();
+    }
+
     public void addContacto(){
         FacesContext context = FacesContext.getCurrentInstance();
         ConContactos con= new ConContactos();
         
-        con.setConEstado('P');
-        con.setConExito(false);
+        if (this.contactoSelected.getConId()!= null){ //modificacion
+            con.setConId(this.contactoSelected.getConId());
+            con.setConEstado(this.contactoSelected.getConEstado());
+            con.setConExito(this.contactoSelected.getConExito());
+        }
+        else //nuevo
+        {
+            con.setConEstado('P');
+            con.setConExito(false);
+        }
         con.setConFechaContacto(this.contactoSelected.getConFechaContacto());
         con.setConIdPersonaCont(this.contactoSelected.getConIdPersonaCont());
+        con.setConIdUsuarioCont(this.contactoSelected.getConIdUsuarioCont());
         con.setConObservacion(this.contactoSelected.getConObservacion());
         
         contactoEJB.update(con);
         context.addMessage("Mensaje", new FacesMessage("Felicidades! " + "El contacto fue agregado con éxito."));
-        listaContactos = contactoEJB.getAll();
+        contactos = contactoEJB.getAll();
         this.clean();
     }
     
     public void deleteContacto(){
         contactoEJB.delete(contactoSelected);
-        listaContactos = contactoEJB.getAll();
+        contactos = contactoEJB.getAll();
         RequestContext.getCurrentInstance().update("contacto-form:dtContacto");
     }
     
@@ -70,12 +116,12 @@ public class ContactoBean implements Serializable{
         RequestContext.getCurrentInstance().update("contacto-form:dtContacto");
     }
 
-    public List<ConContactos> getListaContactos() {
-        return listaContactos;
+    public List<ConContactos> getContactos() {
+        return contactos;
     }
 
-    public void setListaContactos(List<ConContactos> listaContactos) {
-        this.listaContactos = listaContactos;
+    public void setContactos(List<ConContactos> contactos) {
+        this.contactos = contactos;
     }
 
     public ConContactos getContactoSelected() {
@@ -86,12 +132,20 @@ public class ContactoBean implements Serializable{
         this.contactoSelected = contactoSelected;
     }
 
-    public List<PgePersonas> getListaPersonas() {
-        return listaPersonas;
+    public List<PgePersonas> getPersonas() {
+        return personas;
     }
 
-    public void setListaPersonas(List<PgePersonas> listaPersonas) {
-        this.listaPersonas = listaPersonas;
+    public void setPersonas(List<PgePersonas> personas) {
+        this.personas = personas;
+    }
+
+    public List<PgeUsuarios> getUsuarios() {
+        return this.usuarios;
+    }
+
+    public void setUsuarios(List<PgeUsuarios> usuarios) {
+        this.usuarios = usuarios;
     }
 
     public PgePersonas getPersonaSelected() {
@@ -100,6 +154,14 @@ public class ContactoBean implements Serializable{
 
     public void setPersonaSelected(PgePersonas personaSelected) {
         this.personaSelected = personaSelected;
+    }
+
+    public PgeUsuarios getUsuarioSelected() {
+        return this.usuarioSelected;
+    }
+
+    public void setPersonaSelected(PgeUsuarios usuarioSelected) {
+        this.usuarioSelected = usuarioSelected;
     }
 
     public ContactoDao getContactoEJB() {
